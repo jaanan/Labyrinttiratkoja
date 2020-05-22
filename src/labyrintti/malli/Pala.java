0dc4c1166b906451bdfdd398b5f2eae9a75ea076
jaanan <42993030+jaanan@users.noreply.github.com>
2020-05-22 20:13:30 +0300

    Update Pala.java
    Palan koodausta

--- src/labyrintti/malli/Pala.java
+++ src/labyrintti/malli/Pala.java
@@ -1,1 +1,68 @@
+package labyrintti.malli;
 
+// importtaukset
+
+// Tässä luokassa käsitellään yksittäisen labyrintin palasen tietoja
+
+public class Pala {
+
+    public enum Tyyppi {
+        KULKU,
+        MUURI,
+        ULOS;
+    }
+
+    private int rivi;
+    private int sarake;
+    private final Tyyppi tyyppi;
+
+    public Pala(int rivi, int sarake, Tyyppi tyyppi) {
+        this.rivi = rivi;
+        this.sarake = sarake;
+        this.tyyppi = tyyppi;
+    }
+
+    public int getRivi() {
+        return rivi;
+    }
+
+    public int getSarake() {
+        return sarake;
+    }
+
+    public boolean onkoKulku() {
+        return tyyppi == Tyyppi.KULKU;
+    }
+
+    public boolean onkoMuuri() {
+        return tyyppi == Tyyppi.MUURI;
+    }
+
+    public boolean onkoULOS() {
+        return tyyppi == Tyyppi.ULOS;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        var pala = (Pala) o;
+        return rivi == pala.rivi &&
+            sarake == pala.sarake &&
+            tyyppi == pala.tyyppi;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(rivi, sarake, tyyppi);
+    }
+
+    @Override
+    public String toString() {
+        return "Pala{" +
+            "rivi=" + rivi +
+            ", sarake=" + sarake +
+            ", tyyppi=" + tyyppi +
+            '}';
+    }
+}
