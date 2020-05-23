8523d8403e4a399078213715b07ed66bb94f9a70
jaanan <42993030+jaanan@users.noreply.github.com>
2020-05-23 08:53:19 +0300

    Update Solmu.java
    Mistä tässä luokassa on kyse

--- src/labyrintti/algoritmit/ratkaisu/Solmu.java
+++ src/labyrintti/algoritmit/ratkaisu/Solmu.java
@@ -1,1 +1,11 @@
+package labyrintti.algoritmit.ratkaisu;
 
+// importit
+
+// Tässä luokassa lasketaan reitin pituutta sisäänkäynniltä eteenpäin
+
+class Solmu {
+  
+  private static int RAJAKUSTANNUS = 1;
+  
+}
